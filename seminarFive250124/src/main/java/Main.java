de23a675eaf0cb69351e6570380e25a3cf51209b
Roman Annenkov <roman.annenkov@gmail.com>
2024-01-28 15:58:14 +0300

    first task from hw of seminar five added

--- seminarFive250124/src/main/java/Main.java
+++ seminarFive250124/src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hw from seminar five");
+        // 1. Написать функцию, создающую резервную копию всех файлов
+        // в директории(без поддиректорий) во вновь созданную папку ./backup
+        try {
+            backupAllFilesFromDirectory(".");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void backupAllFilesFromDirectory(String pathToDirectory) throws IOException {
+        Path bkpDir = Files.createDirectories(Path.of(pathToDirectory + File.separator + "backup"));
+        for (File f : (Objects.requireNonNull(bkpDir.getParent().toFile().listFiles()))) {
+            if (f.isFile()) {
+                Files.copy(f.toPath(),
+                        Path.of(bkpDir.toAbsolutePath() + File.separator + f.getName()),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
     }
 }

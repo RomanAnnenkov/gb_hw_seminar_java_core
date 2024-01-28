b642bcaf4ee19ad64d749df8ce1303929a5e204b
Roman Annenkov <roman.annenkov@gmail.com>
2024-01-28 17:36:57 +0300

    second task from hw of seminar five added

--- seminarFive250124/src/main/java/Main.java
+++ seminarFive250124/src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -12,6 +11,35 @@ public class Main {
         // в директории(без поддиректорий) во вновь созданную папку ./backup
         try {
             backupAllFilesFromDirectory(".");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Предположить, что числа в исходном массиве из 9 элементов имеют диапазон[0, 3],
+        // и представляют собой, например, состояния ячеек поля для игры в крестикинолики,
+        // где 0 – это пустое поле, 1 – это поле с крестиком, 2 – это поле с ноликом, 3 – резервное значение.
+        // Такое предположение позволит хранить в одном числе типа int всё поле 3х3. Записать в файл 9 значений так,
+        // чтобы они заняли три байта.
+        int[] intArray = new int[]{0, 2, 0, 1, 2, 1, 0, 2, 3};
+        saveArrayToFile(intArray, "array_file");
+
+    }
+
+
+    public static void saveArrayToFile(int[] arr, String fileName) {
+        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileName))) {
+            int counter = 0;
+            byte capacitor = 0;
+            for (int element : arr) {
+                if (counter == 3) {
+                    dataOutputStream.writeByte(capacitor);
+                    capacitor = 0;
+                    counter = 0;
+                }
+                capacitor += element << (counter * 2);
+                counter++;
+            }
+            dataOutputStream.writeByte(capacitor);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
